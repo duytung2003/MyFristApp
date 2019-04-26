f832fb85ca601351dc504596645fc4341cb3d6c3
Tung <tungddgch@fpt.edu.vn>
2019-04-26 17:13:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,95 +1,28 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Dog</title>
-</head>
-<body style="margin: auto;width: 700px">
-<h1>Dog D.Dog</h1>
-<table>
-	<tr>
-		<th style="text-align: left; background-color: #A1C0F3; color: blue; font-size: 200%" width="80%" height="20px">About</th>
-		<th rowspan="4"><img src="images/Dog.jpg"></th>
-	</tr>
-	<tr>
-		<td>
-			Woof woof! <br>
-			Hi, I'm Dog. I love <span style="color: blue">User Interface Design</span>
-		</td>
-	</tr>
-	<tr>
-		<th style="text-align: left; background-color: #A1C0F3; color: blue; font-size: 200%" width="80%" height="20px">Course</th>
-	</tr>
-	<tr>
-		<td>
-			Here are some course I've taken:
-		</td>
-	</tr>
-	<tr>
-		<table border="1" cellspacing="0">
-			<tr>
-				<th>Course Number</th>
-				<th>Course Title</th>
-				<th>Semester Taken</th>
-			</tr>
-			<tr>
-				<th align="center" colspan="3">For Credit</th>
-			</tr>
-			<tr>
-				<td style="color: #1F1C8E">6.813</td>
-				<td style="color: blue">User Interface Design</td>
-				<td style="color: blue">Spring 2014</td>
-			</tr>
-			<tr>
-				<td style="color: #1F1C8E">6.034</td>
-				<td>Artificial Interlligence</td>
-				<td>Fall 2013</td>
-			</tr>
-			<tr>
-				<td style="color: #1F1C8E">6.006</td>
-				<td>Introduction to Algorithms</td>
-				<td>Fall 2013</td>
-			</tr>
-			<tr>
-				<th align="center" colspan="3">Listener</th>
-			</tr>
-			<tr>
-				<td style="color: #B4CCF7">6.867</td>
-				<td>Machine Learning</td>
-				<td>Fall 2013</td>
-			</tr>
-			<tr>
-				<td style="color: #B4CCF7">6.830</td>
-				<td>Database Systems</td>
-				<td>Fall 2012</td>
-			</tr>
-		</table>
-	</tr>
-</table>
-<table>
-	<tr>
-		<th style="text-align: left; background-color: #A1C0F3; color: blue; font-size: 200%" width="50%" height="20px">Interests</th>
-	</tr>
-	<tr>
-		<td>
-			<p>I am interested <b>HCI</b> memes, and youtube videos</p>
-		</td>
-	</tr>
-	<tr>
-		<th style="text-align: left; background-color: #A1C0F3; color: blue; font-size: 200%" width="30%" height="20px">Submit Pet Information</th>
-	</tr>
-	<tr>
-		<td>
-			<p>
-				Would love to know what kinds of pets you 'own'(you'll never own me). Submit bellow!
-			</p>
-			<p>
-				Name: <input type="text" name="Name"><br>
-				Phylum: <input type="text" name="Phylum"><br>
-				Gender: <input type="text" name="Gender"><br>
-				<input type="submit" name="Submit">
-			</p>
-		</td>
-	</tr>
-</table>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php 
+$sql = "SELECT id, name FROM label";
+$db = parse_url(getenv("DATABASE_URL"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+foreach ($resultSet as $row) {
+	echo $row['name'] . '\n';
+}
+?>
 </body>
 </html>
