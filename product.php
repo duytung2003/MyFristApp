cfc12d68596d24419d4c4f7fcb86bcf81f28b9ad
Tung <tungddgch@fpt.edu.vn>
2019-05-04 08:48:25 +0700

    Update product.php

--- product.php
+++ product.php
@@ -193,6 +193,18 @@
               $image_category = $rowcategory['image'];
               ?>
             <a href="information.php?proId=<?= $id_categorydb; ?>" ><?= $name_category; ?></a>
+            <?php 
+            $queryproduct = "SELECT proid, proname, image, descrip, price FROM product WHERE id_categorydb = '{$id_categorydb}' ORDER BY proid;";
+            $result = pg_query($connection,$queryproduct);
+            if (pg_num_rows($result) > 0) {
+            // output data of each row
+            while($rowproduct = pg_fetch_assoc($result)) {
+              $id_categorydb = $rowcategory['proid'];
+              $name_category = $rowcategory['proname'];
+              $descrip_category = $rowcategory['descrip'];
+              $price_category = $rowcategory['price'];
+              $image_category = $rowcategory['image'];
+             ?>
             <div class="ads1">
 					<div class="title">
 						<b><?= $name_category; ?></b>
