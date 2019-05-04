2d50c611cd59ad5b3f9943b4d026f300a09d24ab
Tung <tungddgch@fpt.edu.vn>
2019-05-04 11:21:58 +0700

    php

--- Vest.php
+++ Vest.php
@@ -13,7 +13,7 @@
 	<nav class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<img src="img/biluxury.png" style="height: 40px; width: 150px; margin: 5px;">
+				<a href="index.php"><img src="img/biluxury.png" style="height: 40px; width: 150px; margin: 5px;"></a>
 				    </div>
 				    	<ul class="nav navbar-nav">
 				      		<li class="active"><a href="index.php">Home</a></li>
